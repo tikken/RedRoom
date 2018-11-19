06df05f2eb772276dd419f838483e42487ed8cc7
tikken <tikken23@gmail.com>
2018-11-19 23:36:18 +0200

    Add files via upload

---
+++ redRoom 2/script.js
@@ -0,0 +1,60 @@
+ymaps.ready(init);
+
+function init () {
+    var map = new ymaps.Map('map', {
+            center: [54.721432, 20.526579],
+            zoom: 16
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        counter = 0,
+
+        // Создание макета содержимого балуна.
+        // Макет создается с помощью фабрики макетов с помощью текстового шаблона.
+        BalloonContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="margin: 10px;">' +
+                '<b>{{properties.name}}</b><br />' +
+                '<i id="count"></i> ' +
+                '<button id="counter-button"> +1 </button>' +
+            '</div>', {
+
+            // Переопределяем функцию build, чтобы при создании макета начинать
+            // слушать событие click на кнопке-счетчике.
+            build: function () {
+                // Сначала вызываем метод build родительского класса.
+                BalloonContentLayout.superclass.build.call(this);
+                // А затем выполняем дополнительные действия.
+                $('#counter-button').bind('click', this.onCounterClick);
+                $('#count').html(counter);
+            },
+
+            // Аналогично переопределяем функцию clear, чтобы снять
+            // прослушивание клика при удалении макета с карты.
+            clear: function () {
+                // Выполняем действия в обратном порядке - сначала снимаем слушателя,
+                // а потом вызываем метод clear родительского класса.
+                $('#counter-button').unbind('click', this.onCounterClick);
+                BalloonContentLayout.superclass.clear.call(this);
+            },
+
+            onCounterClick: function () {
+                $('#count').html(++counter);
+                if (counter == 5) {
+                    alert('Вы славно потрудились.');
+                    counter = 0;
+                    $('#count').html(counter);
+                }
+            }
+        });
+
+    var placemark = new ymaps.Placemark([54.721432, 20.526579], {
+            name: 'Считаем'
+        }, {
+            balloonContentLayout: BalloonContentLayout,
+            // Запретим замену обычного балуна на балун-панель.
+            // Если не указывать эту опцию, на картах маленького размера откроется балун-панель.
+            balloonPanelMaxMapArea: 0
+        });
+
+    map.geoObjects.add(placemark);
+}
