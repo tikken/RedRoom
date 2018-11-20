4d9dfe363a908477245a11ddf2f15654eef2985c
tikken <tikken23@gmail.com>
2018-11-20 02:26:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var map;
+DG.then(function () {
+    map = DG.map('map', {
+        center: [54.72, 20.52],
+        zoom: 16
+    });
+    DG.marker([54.7214, 20.5265], {label: "Студия звукозаписи Red Room"}).addTo(map).bindPopup('Заблудился? Звони: +7(900)567-97-48', { static: true });
+});
